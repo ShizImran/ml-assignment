6e7e085c042442cf23f09f6c52b6e1eee030e106
Sheeza Imran <sheezaimran24@gmail.com>
2025-06-02 11:58:51 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,10 @@ import os
 # ---------------------
 # Load MLflow model
 # ---------------------
-mlflow_model_path = r"C:\ml_app_assignment\ml\mlruns\160842011003966785\b2bebc2678ba4865975455840df6df96\artifacts\mlflow_model"
+#mlflow_model_path = r"C:\ml_app_assignment\ml\mlruns\160842011003966785\b2bebc2678ba4865975455840df6df96\artifacts\mlflow_model"
 
 try:
+    mlflow_model_path = "./mlflow_model"
     model = mlflow.sklearn.load_model(mlflow_model_path)
 except Exception as e:
     raise RuntimeError(f"Failed to load model from MLflow path: {e}")
